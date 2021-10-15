981fd90e5d9a81cfcb113f1e7164845c2ba87111
quimi <quimikik@hotmail.com>
2021-10-15 18:53:50 +0200

    com1

--- Ejemplo1/main.py
+++ Ejemplo1/main.py
@@ -1,11 +1,8 @@
 import time
-from datetime import datetime
 
-# while True:
-    # Print current time and sleep 1 minute
-    def display():
-        current_time = now.strftime("%H:%M:%S")
-        current_date = today.strftime("%d %B, %Y")
-        print("Today's date: ", current_date,"and ", "current time: ", current_time)
-    t = Timer(60, display)
-    t.start()
+while True:
+    localtime = time.localtime()
+    result = time.strftime("%I:%M:%S %p", localtime)
+    print(result)
+    time.sleep(1=)
+
